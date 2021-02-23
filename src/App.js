66201baf08b0657336c88fb36598f5fb5ab16cd0
Oliver Grohe <o.grohe@gmail.com>
2021-02-23 07:32:36 +0100

    lab

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import Navbar from './Navbar'
+import FormField from './FormField'
+import CoolButton from './Button'
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+  <div>
+    <Navbar />
+    <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+    <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" /> 
+    <CoolButton />
+  </div>
+  )
 };
 
 export default App;
+
